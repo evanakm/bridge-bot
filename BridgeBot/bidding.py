009cb1f1cbfa12fc7adb2a303d959a932b5f1d0c
Evan Meikleham <evanakm@gmail.com>
2019-07-01 18:46:14 -0600

    Cleaning up bidding

--- BridgeBot/bidding.py
+++ BridgeBot/bidding.py
@@ -1,5 +1,3 @@
-from cards import suits
-
 bids = ['1C',' 1D', '1H', '1S', '1N',
         '2C', '2D', '2H', '2S', '2N',
         '3C', '3D', '3H', '3S', '3N',
@@ -8,23 +6,32 @@ bids = ['1C',' 1D', '1H', '1S', '1N',
         '6C', '6D', '6H', '6S', '6N',
         '7C', '7D', '7H', '7S', '7N']
 
-calls = ['PASS','X','XX']
-
 players = ['NORTH', 'EAST', 'SOUTH', 'WEST']
+
+strains = ['CLUBS', 'DIAMONDS', 'HEARTS', 'SPADES', 'NT', 'PASSOUT']
+
+doubles = [None, 'X', 'XX']
+
+ps = 'PASS'
 
 
 class Auction:
+    contract = {'level': 0,
+                'strain': None,
+                'doubled': None,
+                'declarer': None}
+
     def __init__(self, dealer):
-        dealer = dealer.upper()
+        self.dealer = dealer.upper()
 
         if not dealer in players:
             raise Exception('Invalid dealer.')
 
         self.record = {
-            'NORTH':[],
-            'EAST':[],
-            'SOUTH':[],
-            'WEST':[]
+            'NORTH': [],
+            'EAST': [],
+            'SOUTH': [],
+            'WEST': []
         }
 
         self.ns_first_bid = {
@@ -36,14 +43,14 @@ class Auction:
         }
 
         self.ew_first_bid = {
-            'CLUBS':"",
-            'DIAMONDS':"",
-            'HEARTS':"",
-            'SPADES':"",
-            'NT':""
+            'CLUBS': "",
+            'DIAMONDS': "",
+            'HEARTS': "",
+            'SPADES': "",
+            'NT': ""
         }
 
-        self.player_index = players.index(dealer)
+        self.player_index = players.index(self.dealer)
         self.last_bidder_index = None
 
         for i in range(self.player_index):
@@ -54,69 +61,71 @@ class Auction:
         self.redoubled = False
         self.consecutive_passes = 0
 
+    # For debugging purposes, easier to reset than to create a new Auction
+    def reset(self):
+        contract = {'level': 0,
+                    'strain': None,
+                    'doubled': None,
+                    'declarer': None}
+
+        self.player_index = players.index(self.dealer)
+        self.last_bidder_index = None
+
+        for i in range(self.player_index):
+            self.record[players[i]].append("-")
+
+        self.bid_index = -1
+        self.doubled_by = None # Redundant with contract dictionary, but makes the logic below much cleaner.
+        self.redoubled = False
+        self.consecutive_passes = 0
+
     def __increment_player(self):
         self.player_index = (self.player_index + 1) % 4
 
+    # Bids return either "DONE" or "CONTINUE" depending on whether the auction is finished,
+    # or "INVALID" if the bid is not allowed.
+    ret_val = ["DONE", "CONTINUE", "INVALID"]
+
     def pass_bid(self):
         if self.bid_index == -1:
-            if self.consecutive_passes == 3:
-                self.record[players[self.player_index]].append(calls[0])
-                return "PASSOUT"
-            else:
-                self.record[players[self.player_index]].append(calls[0])
+            if self.consecutive_passes == 3: # Passing out
+                self.record[players[self.player_index]].append(ps)
+                self.contract['strain'] = strains[5]
+                return self.ret_val[0]
+            else:  # Passing before an opening bid
+                self.record[players[self.player_index]].append(ps)
                 self.__increment_player()
-                return "CONTINUE"
-        elif self.consecutive_passes != 2:
-            self.record[players[self.player_index]].append(calls[0])
+                return self.ret_val[1]  # "CONTINUE"
+        elif self.consecutive_passes != 2: # Passing but not finishing
+            self.record[players[self.player_index]].append(ps)
             self.__increment_player()
-            return "CONTINUE"
-        else:
+            return self.ret_val[1]  # "CONTINUE"
+        else:  # Three passes in a row.
             if self.redoubled:
-                contract = " " + calls[2]
+                self.contract['doubled'] = doubles[2]
             elif self.doubled_by:
-                contract = " " + calls[1]
+                self.contract['doubled'] = doubles[1]
             else:
-                contract = ""
+                self.contract['doubled'] = doubles[0]
 
-            contract = "CONTRACT: " + bids[self.bid_index] + contract
+            self.contract['strain'] = strains[self.bid_index % 5]
+            self.contract['level'] = int( 1 + (self.bid_index / 5) )
 
-            declarer = " BY "
+            # Kept track of who was the first to bid a strain
+            if self.last_bidder_index % 2 == 0:
+                self.contract['declarer'] = self.ns_first_bid[self.contract['strain']]
+            else:
+                self.contract['declarer'] = self.ew_first_bid[self.contract['strain']]
 
-            if self.bid_index % 5 == 0:
-                if self.last_bidder_index % 2 == 0:
-                    declarer = declarer + self.ns_first_bid["CLUBS"]
-                else:
-                    declarer = declarer + self.ew_first_bid["CLUBS"]
-            elif self.bid_index % 5 == 1:
-                if self.last_bidder_index % 2 == 0:
-                    declarer = declarer + self.ns_first_bid["DIAMONDS"]
-                else:
-                    declarer = declarer + self.ew_first_bid["DIAMONDS"]
-            elif self.bid_index % 5 == 2:
-                if self.last_bidder_index % 2 == 0:
-                    declarer = declarer + self.ns_first_bid["HEARTS"]
-                else:
-                    declarer = declarer + self.ew_first_bid["HEARTS"]
-            elif self.bid_index % 5 == 3:
-                if self.last_bidder_index % 2 == 0:
-                    declarer = declarer + self.ns_first_bid["SPADES"]
-                else:
-                    declarer = declarer + self.ew_first_bid["SPADES"]
-            elif self.bid_index % 5 == 4:
-                if self.last_bidder_index % 2 == 0:
-                    declarer = declarer + self.ns_first_bid["NT"]
-                else:
-                    declarer = declarer + self.ew_first_bid["NT"]
-
-            self.record[players[self.player_index]].append(calls[0])
-            return contract + declarer
+            self.record[players[self.player_index]].append(ps)
+            return self.ret_val[0]  # "DONE"
 
     def make_bid(self,bid_index):
-        if not bid_index in range(35):
-            raise Exception("Invalid bid index.")
+        if bid_index not in range(35):
+            return self.ret_val[2]  # "INVALID"
 
         if bid_index <= self.bid_index:
-            return "INSUFFICIENT"
+            return self.ret_val[2]  # "INVALID"
 
         self.bid_index = bid_index
         self.doubled_by = None
@@ -124,39 +133,39 @@ class Auction:
         self.consecutive_passes = 0
 
         self.record[players[self.player_index]].append(bids[bid_index])
-        return "CONTINUE"
+        return self.ret_val[1]  # "CONTINUE"
 
     def double(self):
-        if self.bid_index == -1: #No bids yet
-            return "INSUFFICIENT"
+        if self.bid_index == -1:  # No bids yet
+            return self.ret_val[2]  # "INVALID"
         elif self.redoubled:
-            return "INSUFFICIENT"
-        elif self.player_index % 2 == 0 and self.doubled_by == "NS": #Already doubled
-            return "INSUFFICIENT"
-        elif self.player_index % 2 == 1 and self.doubled_by == "EW": #Already doubled
-            return "INSUFFICIENT"
-        elif self.last_bidder_index % 2 == self.player_index % 2: #Can't double yourself
-            return "INSUFFICIENT"
+            return self.ret_val[2]  # "INVALID"
+        elif self.player_index % 2 == 0 and self.doubled_by == "NS":  # Already doubled
+            return self.ret_val[2]  # "INVALID"
+        elif self.player_index % 2 == 1 and self.doubled_by == "EW":  # Already doubled
+            return self.ret_val[2]  # "INVALID"
+        elif self.last_bidder_index % 2 == self.player_index % 2:  # Can't double yourself
+            return self.ret_val[2]  # "INVALID"
         else:
             if self.player_index % 2 == 0:
                 self.doubled_by = "NS"
-                self.record[players[self.player_index]].append(calls[1])
-                return "CONTINUE"
+                self.record[players[self.player_index]].append(doubles[1])
+                return self.ret_val[1]  # "CONTINUE"
             else:
                 self.doubled_by = "EW"
-                self.record[players[self.player_index]].append(calls[1])
-                return "CONTINUE"
+                self.record[players[self.player_index]].append(doubles[1])
+                return self.ret_val[1]  # "CONTINUE"
 
     def redouble(self):
-        if not self.doubled_by: #Can't redouble unless first doubled
-            return "INSUFFICIENT"
-        elif self.doubled_by == "NS" and self.player_index % 2 == 0: #Can only redouble opponent's bid
-            return "INSUFFICIENT"
-        elif self.doubled_by == "EW" and self.player_index % 2 == 1: #Can only redouble opponent's bid
-            return "INSUFFICIENT"
+        if not self.doubled_by:  # Can't redouble unless first doubled
+            return self.ret_val[2]  # "INVALID"
+        elif self.doubled_by == "NS" and self.player_index % 2 == 0:  # Can only redouble opponent's double
+            return self.ret_val[2]  # "INVALID"
+        elif self.doubled_by == "EW" and self.player_index % 2 == 1:  # Can only redouble opponent's double
+            return self.ret_val[2]  # "INVALID"
         else:
             self.doubled_by = None
             self.redoubled = True
-            self.record[players[self.player_index]].append(calls[2])
-            return "CONTINUE"
+            self.record[players[self.player_index]].append(doubles[2])
+            return self.ret_val[1]  # "CONTINUE"
 
