6e234669c8c674db7e7e7353dab94e4cb2bc7c77
Mason Brothers <brothers.mason@gmail.com>
2019-07-18 11:48:48 -0600

    Add space for play_card inputs

--- BridgeBot/cardplay.py
+++ BridgeBot/cardplay.py
@@ -1,23 +1,14 @@
 from BridgeBot.enums import Strains, strains
 
-from BridgeBot.enums import Strains, strains, Players
-
-
-contracts = ['1C',' 1D', '1H', '1S', '1N',
-        '2C', '2D', '2H', '2S', '2N',
-        '3C', '3D', '3H', '3S', '3N',
-        '4C', '4D', '4H', '4S', '4N',
-        '5C', '5D', '5H', '5S', '5N',
-        '6C', '6D', '6H', '6S', '6N',
-        '7C', '7D', '7H', '7S', '7N']
+from BridgeBot.enums import Strains, strains, Players, contracts
 
 players = [Players.NORTH, Players.EAST, Players.SOUTH, Players.WEST]
 
 INVALID = "INVALID"
 
 def play_card():
-    suit = input("Enter suit:")
-    rank = input("Enter rank:")
+    suit = input("Enter suit: ")
+    rank = input("Enter rank: ")
     return suit, rank
 
 class Cardplay:
