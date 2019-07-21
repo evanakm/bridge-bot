f9b8872c2a3e97174e1c212c96b8ad45d334b51d
Mason Brothers <brothers.mason@gmail.com>
2019-07-20 19:43:36 -0600

    Set play_trick function to the tested one

--- BridgeBot/cardplay.py
+++ BridgeBot/cardplay.py
@@ -51,58 +51,9 @@ class CardPlay:
 
         return played_cards.index(highest_card)
 
-
-
-    # played_cards must be an ordered structure
-    # notrump corresponds to trump_index == 4,
+    
     def play_trick(self, played_cards):
-
-        lead_card = played_cards[0]
-        following_cards = played_cards[1:len(played_cards)]
-
-        # Compare suit_index to trump_index so that there's no
-        # edge cases with respect to strains. If playing No Trump, trump_index
-        # will be 4, and thus never equal to suit_index (in 0, 1, 2, 3)
-        trump_played = lead_card.suit == self.trump_suit
-
-        winning_index = 0
-        counter = 0
-        suit_led = lead_card.suit
-        highest = lead_card.rank # Easier to compare indices
-
-        for card in following_cards:
-            counter = counter + 1
-            if trump_played:
-                if card.suit != self.trump_suit:
-                    continue #Not following trump. Not winning trick.
-                elif card.rank <= highest:
-                    continue #Following low.
-                else:
-                    winning_index = counter
-                    highest = card.rank_index
-            elif card.suit_index == suit_led:
-                if card.rank_index <= highest:
-                    continue
-                else:
-                    winning_index = counter
-                    highest = card.rank_index
-            else:
-                if card.suit == self.trump_suit:
-                    if not trump_played:
-                        # first time the lead is trumped
-                        trump_played = True
-                        winning_index = counter
-                        highest = card.rank_index
-                    elif card.rank_index <= highest:
-                        continue
-                    else:
-                        winning_index = counter
-                        highest = card.rank_index
-                else:
-                    # Not following suit, not trumping
-                    continue
-
-        return winning_index
+        return self.determine_trick_winner(played_cards, self.trump_suit)
 
     def __init__(self, hands, contract, declarer):
         if not isinstance(declarer, Players):
