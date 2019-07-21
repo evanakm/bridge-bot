dba1ae996e584a1d2f9e5165db88386652484cfa
Mason Brothers <brothers.mason@gmail.com>
2019-07-20 23:50:29 -0600

    Parameterized tests

--- BridgeBot/test/test_cardplay.py
+++ BridgeBot/test/test_cardplay.py
@@ -9,114 +9,117 @@ from enums import Suits, Ranks
 from cards import Card
 
 
-
-def test_determine_trick_winner():
-    assert cardplay.CardPlay.determine_trick_winner(
-        [
-            Card(Suits.SPADES, Ranks.FOUR),
-            Card(Suits.HEARTS, Ranks.ACE),
-            Card(Suits.CLUBS, Ranks.ACE),
-            Card(Suits.DIAMONDS, Ranks.ACE)
-        ],
-        Suits.SPADES
-    ) == 0
-
-    assert cardplay.CardPlay.determine_trick_winner(
-        [
-            Card(Suits.HEARTS, Ranks.ACE),
-            Card(Suits.SPADES, Ranks.FOUR),
-            Card(Suits.CLUBS, Ranks.ACE),
-            Card(Suits.DIAMONDS, Ranks.ACE)
-        ],
-        Suits.SPADES
-    ) == 1
-
-    assert cardplay.CardPlay.determine_trick_winner(
-        [
-            Card(Suits.HEARTS, Ranks.ACE),
-            Card(Suits.CLUBS, Ranks.ACE),
-            Card(Suits.DIAMONDS, Ranks.ACE),
-            Card(Suits.SPADES, Ranks.FOUR)
-        ],
-        Suits.SPADES
-    ) == 3
-
-    assert cardplay.CardPlay.determine_trick_winner(
-        [
-            Card(Suits.SPADES, Ranks.FOUR),
-            Card(Suits.HEARTS, Ranks.FIVE),
-            Card(Suits.CLUBS, Ranks.ACE),
-            Card(Suits.DIAMONDS, Ranks.ACE)
-        ],
-        Suits.HEARTS
-    ) == 1
-
-    assert cardplay.CardPlay.determine_trick_winner(
-        [
-            Card(Suits.SPADES, Ranks.FOUR),
-            Card(Suits.HEARTS, Ranks.FIVE),
-            Card(Suits.CLUBS, Ranks.ACE),
-            Card(Suits.DIAMONDS, Ranks.ACE)
-        ],
-        Suits.DIAMONDS
-    ) == 3
-
-    assert cardplay.CardPlay.determine_trick_winner(
-        [
-            Card(Suits.SPADES, Ranks.FOUR),
-            Card(Suits.DIAMONDS, Ranks.FIVE),
-            Card(Suits.CLUBS, Ranks.ACE),
-            Card(Suits.DIAMONDS, Ranks.ACE)
-        ],
-        Suits.DIAMONDS
-    ) == 3
-
-    assert cardplay.CardPlay.determine_trick_winner(
-        [
-            Card(Suits.DIAMONDS, Ranks.ACE),
-            Card(Suits.SPADES, Ranks.FOUR),
-            Card(Suits.DIAMONDS, Ranks.FIVE),
-            Card(Suits.CLUBS, Ranks.ACE),
-        ],
-        Suits.DIAMONDS
-    ) == 0
-
-    assert cardplay.CardPlay.determine_trick_winner(
-        [
-            Card(Suits.SPADES, Ranks.FOUR),
-            Card(Suits.DIAMONDS, Ranks.ACE),
-            Card(Suits.DIAMONDS, Ranks.FIVE),
-            Card(Suits.CLUBS, Ranks.ACE),
-        ],
-        Suits.DIAMONDS
-    ) == 1
-
-    assert cardplay.CardPlay.determine_trick_winner(
-        [
-            Card(Suits.SPADES, Ranks.FOUR),
-            Card(Suits.DIAMONDS, Ranks.FIVE),
-            Card(Suits.DIAMONDS, Ranks.ACE),
-            Card(Suits.CLUBS, Ranks.ACE),
-        ],
-        Suits.DIAMONDS
-    ) == 2
-
-    assert cardplay.CardPlay.determine_trick_winner(
-        [
-            Card(Suits.CLUBS, Ranks.ACE),
-            Card(Suits.SPADES, Ranks.FOUR),
-            Card(Suits.DIAMONDS, Ranks.FIVE),
-            Card(Suits.DIAMONDS, Ranks.ACE),
-        ],
-        Suits.DIAMONDS
-    ) == 3
-
-    assert cardplay.CardPlay.determine_trick_winner(
-        [
-            Card(Suits.CLUBS, Ranks.ACE),
-            Card(Suits.SPADES, Ranks.FOUR),
-            Card(Suits.DIAMONDS, Ranks.ACE),
-            Card(Suits.DIAMONDS, Ranks.FIVE),
-        ],
-        Suits.DIAMONDS
-    ) == 2
+@pytest.mark.parametrize('played_cards, trump_suit, expected', [
+    (
+            [
+                Card(Suits.SPADES, Ranks.FOUR),
+                Card(Suits.HEARTS, Ranks.ACE),
+                Card(Suits.CLUBS, Ranks.ACE),
+                Card(Suits.DIAMONDS, Ranks.ACE)
+            ],
+            Suits.SPADES,
+            0
+    ),
+    (
+            [
+                Card(Suits.HEARTS, Ranks.ACE),
+                Card(Suits.SPADES, Ranks.FOUR),
+                Card(Suits.CLUBS, Ranks.ACE),
+                Card(Suits.DIAMONDS, Ranks.ACE)
+            ],
+            Suits.SPADES,
+            1
+    ),
+    (
+            [
+                Card(Suits.HEARTS, Ranks.ACE),
+                Card(Suits.CLUBS, Ranks.ACE),
+                Card(Suits.DIAMONDS, Ranks.ACE),
+                Card(Suits.SPADES, Ranks.FOUR)
+            ],
+            Suits.SPADES,
+            3
+    ),
+    (
+            [
+                Card(Suits.SPADES, Ranks.FOUR),
+                Card(Suits.HEARTS, Ranks.FIVE),
+                Card(Suits.CLUBS, Ranks.ACE),
+                Card(Suits.DIAMONDS, Ranks.ACE)
+            ],
+            Suits.HEARTS,
+            1
+    ),
+    (
+            [
+                Card(Suits.SPADES, Ranks.FOUR),
+                Card(Suits.HEARTS, Ranks.FIVE),
+                Card(Suits.CLUBS, Ranks.ACE),
+                Card(Suits.DIAMONDS, Ranks.ACE)
+            ],
+            Suits.DIAMONDS,
+            3
+    ),
+    (
+            [
+                Card(Suits.SPADES, Ranks.FOUR),
+                Card(Suits.DIAMONDS, Ranks.FIVE),
+                Card(Suits.CLUBS, Ranks.ACE),
+                Card(Suits.DIAMONDS, Ranks.ACE)
+            ],
+            Suits.DIAMONDS,
+            3
+    ),
+    (
+            [
+                Card(Suits.DIAMONDS, Ranks.ACE),
+                Card(Suits.SPADES, Ranks.FOUR),
+                Card(Suits.DIAMONDS, Ranks.FIVE),
+                Card(Suits.CLUBS, Ranks.ACE),
+            ],
+            Suits.DIAMONDS,
+            0
+    ),
+    (
+            [
+                Card(Suits.SPADES, Ranks.FOUR),
+                Card(Suits.DIAMONDS, Ranks.ACE),
+                Card(Suits.DIAMONDS, Ranks.FIVE),
+                Card(Suits.CLUBS, Ranks.ACE),
+            ],
+            Suits.DIAMONDS,
+            1
+    ),
+    (
+            [
+                Card(Suits.SPADES, Ranks.FOUR),
+                Card(Suits.DIAMONDS, Ranks.FIVE),
+                Card(Suits.DIAMONDS, Ranks.ACE),
+                Card(Suits.CLUBS, Ranks.ACE),
+            ],
+            Suits.DIAMONDS,
+            2
+    ),
+    (
+            [
+                Card(Suits.CLUBS, Ranks.ACE),
+                Card(Suits.SPADES, Ranks.FOUR),
+                Card(Suits.DIAMONDS, Ranks.FIVE),
+                Card(Suits.DIAMONDS, Ranks.ACE),
+            ],
+            Suits.DIAMONDS,
+            3
+    ),
+    (
+            [
+                Card(Suits.CLUBS, Ranks.ACE),
+                Card(Suits.SPADES, Ranks.FOUR),
+                Card(Suits.DIAMONDS, Ranks.ACE),
+                Card(Suits.DIAMONDS, Ranks.FIVE),
+            ],
+            Suits.DIAMONDS,
+            2
+    )
+])
+def test_determine_trick_winner(played_cards, trump_suit, expected):
+    assert cardplay.CardPlay.determine_trick_winner(played_cards, trump_suit) == expected
