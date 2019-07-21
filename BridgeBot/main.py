63ce3c74284366f1e39e6fad5eca57714499d607
Mason Brothers <brothers.mason@gmail.com>
2019-07-20 19:46:17 -0600

    Remove unused function

--- BridgeBot/main.py
+++ BridgeBot/main.py
@@ -5,17 +5,6 @@ from enums import Players, Vulnerabilities, Contracts
 from cardplay import CardPlay
 from get_input import get_input_enum
 
-def check_cards(cards):
-    if not cards:
-        deck.shuffle()
-        return deck.card_indices
-    elif set(cards) != set(range(52)):
-        print("'cards' must be a list of integers from 0 to 51 with each number appearing once.")
-        print("Generating a random deal.")
-        deck.shuffle()
-        return deck.card_indices
-
-
 if __name__ == "__main__":
     deck = Deck()
     deal = Deal(Players.NORTH, Vulnerabilities.NONE, deck.card_indices)
