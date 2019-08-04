d56603767f2b656dbebeb2316a24e60f61e72256
Mason Brothers <brothers.mason@gmail.com>
2019-08-04 15:40:17 -0600

    Made determine_level a member function

--- bridgebot/scoring.py
+++ bridgebot/scoring.py
@@ -331,7 +331,7 @@ def get_score_from_result(contract, doubled, tricks_taken, vulnerability):
     if contract not in contracts:
         raise ContractNotFound("Invalid contract")
 
-    bid = Contracts.determine_level_from_contract(contract)
+    bid = contract.determine_level()
     strain = contract.determine_strain()
 
     required_number_of_tricks = bid + 6
