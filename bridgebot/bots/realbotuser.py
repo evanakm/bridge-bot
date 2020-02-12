e70673aebc235486357a776473267fdbb5f2d8bc
Mason Brothers <brothers.mason@gmail.com>
2020-02-11 19:16:43 -0700

    Create RealBotUser interface

---
+++ bridgebot/bots/realbotuser.py
@@ -0,0 +1,13 @@
+from game.interface import User
+
+import random
+
+
+class RandomBotUser(User):
+    @staticmethod
+    def play_card(current_player, dummy, dummy_hand, all_cards, legal_cards, bid_history, card_history, leader_history):
+        return random.choice(tuple(legal_cards))
+
+    @staticmethod
+    def make_bid(record, legal_bids):
+        return random.choice(tuple(legal_bids))
